75dfa67a2b5fc4b52004b1bd76ba9e703f4573d6
Kat Tyushnyakova <avchimchik@gmail.com>
2024-09-12 19:22:05 +0500

    Круг изменяет свои размеры

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const square = document.querySelector('#square');
-const inputRange = document.querySelector('input[type=text]');
+const inputText = document.querySelector('input[type=text]');
+const btn = document.querySelector('#btn');
 const eBtn = document.querySelector('#e_btn');
+const inputRange = document.querySelector('input[type=range]');
+const circle = document.querySelector('#circle');
+const span = document.querySelector('#range-span');
 
 const logger = function () {
-    square.style.backgroundColor = inputRange.value;
+    square.style.backgroundColor = inputText.value;
 }
 
-eBtn.addEventListener('click', logger);
+const eventСircle = function (event) {
+    span.textContent = event.target.value;
+    circle.style.width = span.textContent + "%"; 
+    circle.style.height = span.textContent + "%"; 
+}
 
+btn.addEventListener('click', logger);
+
+eBtn.style = "display: none; ";
+
+inputRange.addEventListener('input', eventСircle);
