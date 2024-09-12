2846a3406dbcc5e6067b43b1fa5ed2f2b01157ed
Kat Tyushnyakova <avchimchik@gmail.com>
2024-09-12 18:52:38 +0500

    Квадрат меняет цвет по нажатию кнопки

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const square = document.querySelector('#square');
+const inputRange = document.querySelector('input[type=text]');
+const eBtn = document.querySelector('#e_btn');
 
+const logger = function () {
+    square.style.backgroundColor = inputRange.value;
+}
 
-
-
+eBtn.addEventListener('click', logger);
 
